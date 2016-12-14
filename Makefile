663ea9c047412698d49a750feb3fc759ac391c26
Vlad K <github@acheronmedia.com>
2016-12-14 02:50:30 +0100

    Turn into an installable package.

--- Makefile
+++ Makefile
@@ -2,4 +2,5 @@
 
 deploy:
 	rsync --delete -re ssh ./bsdto niobe.bsdlabs.io:/usr/local/bsdto/
+	rsync -e ssh ./setup.py niobe.bsdlabs.io:/usr/local/bsdto/
 	rsync -e ssh ./requirements.txt niobe.bsdlabs.io:/usr/local/bsdto/
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="BSDTo",
+      version="1.0",
+      description="BSD.to URL shortener",
+      author="Vladimir Krstulja",
+      author_email="vlad-fbsd@acheronmedia.com",
+      packages=["bsdto"]
+)
